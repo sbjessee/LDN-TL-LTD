39d0b2060c7932ff2766726db7af4fb640cfa9ef
Yannik Marchand <ymarchand@me.com>
2026-04-13 22:26:16 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = \
 
 setuptools.setup(
     name = "ldn",
-    version = "0.0.16",
+    version = "0.0.17",
     description = description,
     long_description = long_description,
     author = "Yannik Marchand",
